11ae315d5006b47bf7133f8194ee0d8f1393b0f7
Siddharth-Dhir <95234953+Siddharth-Dhir@users.noreply.github.com>
2022-07-25 20:33:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
-song ="" 
+song ="";
 lWx=0;lWy=0;rWx=0;rWy=0;
 LeftScore = 0;
+RightScore = 0;
+
 
 
     function preload(){
-        song = loadSound("aVeryShortName.mp3")
+        song = loadSound("aVeryShortName.mp3");
         
         }
         function setup(){
-            canvas = createCanvas(600, 500)
+            canvas = createCanvas(600, 500);
             canvas.position(450, 210);
             video = createCapture(VIDEO);
             video.hide();
@@ -18,16 +20,44 @@ LeftScore = 0;
         function draw(){
             image(video, 0, 0, 600, 500);
             fill("red");
-            stroke("darkgray");
+            stroke("darkgrey");
+            
+            circle(rWx, rWy, 22);
             if(LeftScore => 0.3){
 
             
             circle(lWx, lWy, 22);
-            circle(rWx, rWy, 22);
+            
             
             volume =floor(Number(lWy)) / 500;
+            
+            document.getElementById("rD").innerHTML = "Volume: " + volume;  
             song.setVolume(volume);
-            document.getElementById("rD").innerHTML = "Volume: " + volume;
+        }
+        if(rWy > 0 && rWy <= 100){
+            document.getElementById("lD").innerHTML = "Speed = 0.5x";
+            song.rate(0.5);
+
+        }
+        else if(rWy > 100 && rWy <= 200){
+            document.getElementById("lD").innerHTML = "Speed = 1x";
+            song.rate(1);
+
+        }
+        else if(rWy > 200 && rWy <= 300){
+            document.getElementById("lD").innerHTML = "Speed = 1.5x";
+            song.rate(1.5);
+
+        }
+        else if(rWy > 300 && rWy <= 400){
+            document.getElementById("lD").innerHTML = "Speed = 2x";
+            song.rate(2);
+
+        }
+        else if(rWy > 400 && rWy <= 5.00){
+            document.getElementById("lD").innerHTML = "Speed = 3x";
+            song.rate(3);
+
         }
         }
 
@@ -53,6 +83,7 @@ LeftScore = 0;
                 console.log("Left Wrist X(lWx): "+ lWx + ", Left Wrist Y(lWy): " + lWy);
                 console.log("Right Wrist X(rWx): "+ rWx + ", Right Wrist Y(rWy): " + rWy);
                 LeftScore = results[0].pose.keypoints[9].score;
+                RightScore = results[0].pose.keypoints[10].score;
             }
         }
 
